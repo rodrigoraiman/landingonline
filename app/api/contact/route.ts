32ae0bbdb5865eb406d6baf0d5fea0bbd1493e1a
Rodrigo Raiman <rodrigoraiman@MBPdeRodrigo2.lan>
2026-02-05 16:16:45 +0100

    RefactorPrisma

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
 
 export async function POST(req: Request) {
   const data = await req.json();
